5c0f95d81d8896633312c674e697a706eca7c89c
kenenisa <kenenisaboru998@gmail.com>
2026-02-17 22:04:12 +0000

    Update connect.php

--- day2/connect.php
+++ day2/connect.php
@@ -1,17 +1,99 @@
 <?php
-// Database credentials
-$host = "localhost";   // usually localhost
-$user = "root";        // your MySQL username
-$pass = "";            // your MySQL password (leave empty if none)
-$db   = "myclinic";    // the database you created
-
-// Create connection
-$conn = new mysqli($host, $user, $pass, $db);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+/*
+$str = "15";
+$int = 5;
+$result = $str + $int;
+echo "$result";
+define("name", "kenenisa");
+echo name;
+const name="kenenisa";
+echo name;
+define("name", "kenenisa");
+function testGlobal(){
+    echo name;
 }
+    functions in php
+ create a function
+excute the code to be used
+returning the result
+calling the functions
+echo name;
+testGlobal();
+function addtonumbers($num1, $num2){
+    $result = $num1+ $num2;
+    return $result;
+}
+echo addtonumbers(5, 10);
+passing by value
+function add($x,$y){
+    $x=$x +$y;
+    return $x;
+}
+echo add(2,3);
 
-echo "Connected successfully!";
+function addtonumbers($num1, $num2){
+    $result = $num1+ $num2;
+    return $result;
+}
+echo addtonumbers(5, 10);
+passing by value
+function add($x,$y){
+    $x=$x +$y;
+    return $x;
+}
+echo add(2,3);
+ echo date("Y-m-d H:i:s"); 
+ annomous function
+    $number =[1,2,3,4];
+   $squared= array_map(function ($n){
+    return $n** 2;
+   },
+   $number);
+   print_r($squared);
+anynomous function
+ $number=10;
+ $double=function($newnumber)use
+ ($number){
+    return $newnumber * $number;
+ };
+ echo $double(30);
+ function greet($name){
+    return "hellow, " .$name . "!";
+}
+function processUser($callback){
+    $name ="Bro";
+    echo $callback($name);
+}
+processUser("greet");
+//simple callback function=you can pass the name of the function as a string to another function
+// and that function  will be called whe the called back is invoked
+// Function to square a number
+function square($number){
+    return $number *$number;
+}
+function applycallback($numbers, $callback){
+    foreach($numbers as $number){
+        echo $callback($number)."\n";
+    }
+}
+$number=[5,10,25,];
+applycallback($number,"square");
+function qubs($num){
+    return $num * $num * $num;
+}
+function applycallback($number, $callback){
+    foreach($number as $num){
+        echo $callback($num) ."\n";
+    }
+}
+$number=[2,4,6];
+applycallback($number,"qubs");
+*/
+
+
+
+
+
+
+
 ?>
